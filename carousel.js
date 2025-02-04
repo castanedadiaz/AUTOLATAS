d9233b973904d5c3ba1f9fb771f8fd3398bea307
unknown <andresfcastaneda@yahoo.com>
2025-02-03 21:02:23 -0300

    creoqueesta

--- carousel.js
+++ carousel.js
@@ -27,3 +27,33 @@ $(document).ready(function(){
         ]
     });
 });
+
+$(document).ready(function(){
+    $('.carousel2').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
